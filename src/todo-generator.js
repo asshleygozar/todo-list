05230949c5839314e271fecd899dedacf84a8c65
Asshley <asshleydeanwaynegozar@gmail.com>
2025-06-08 19:41:19 +0800

    add todo list dom variables

---
+++ src/todo-generator.js
@@ -0,0 +1,12 @@
+const contentContainer = document.getElementById("content");
+
+// Todo List Card
+const todoBox = document.createElement("div");
+const todoTitle = document.createElement("p");
+const todoDescription = document.createElement("p");
+const todoPriority = document.createElement("select");
+const highPriority = document.createElement("option");
+const mediumPriority = document.createElement("option");
+const lowPriority = document.createElement("option");
+
+
