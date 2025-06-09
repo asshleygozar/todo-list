4c42838292a292d4fc9a922b18180f2baab9c143
Asshley <asshleydeanwaynegozar@gmail.com>
2025-06-09 20:52:35 +0800

    add element variables for todo list cards

--- src/todo-generator.js
+++ src/todo-generator.js
@@ -4,9 +4,24 @@ const contentContainer = document.getElementById("content");
 const todoBox = document.createElement("div");
 const todoTitle = document.createElement("p");
 const todoDescription = document.createElement("p");
-const todoPriority = document.createElement("select");
-const highPriority = document.createElement("option");
+const todoPriority = document.createElement("p");
+const buttonContainer = document.createElement("option");
 const mediumPriority = document.createElement("option");
 const lowPriority = document.createElement("option");
+
+// Todo List Modal Add Card
+const addModalBox = document.createElement("div");
+const addTaskNameLabel = document.createElement("label");
+const addTaskNameInput = document.createElement("input");
+const addTaskDescriptionLabel = document.createElement("label");
+const addTaskDescriptionInput = document.createElement("input");
+const addPriorityLabel = document.createElement("label");
+const addTodoPriority = document.createElement("select");
+const addHighPriority = document.createElement("option");
+const addMediumPriority = document.createElement("option");
+const addLowPriority = document.createElement("option");
+
+
+
 
 
