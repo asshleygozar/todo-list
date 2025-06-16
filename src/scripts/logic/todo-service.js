fb527b4a18831c9329b011f7f47445db2c59bdca
Asshley <asshleydeanwaynegozar@gmail.com>
2025-06-16 18:24:06 +0800

    refactor: place logic modules to logic folder for organization

---
+++ src/scripts/logic/todo-service.js
@@ -0,0 +1,45 @@
+
+import { format } from 'date-fns';
+
+class Todo {
+    constructor(title, description="", dueDate, priority) {
+        this.id = crypto.randomUUID();
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
+
+export class TodoService {
+
+    constructor() {
+        this.todoList = [];
+        
+    }
+
+    saveTodoToUi() {
+
+        const taskTitle = document.getElementById("task-title").value;
+        const taskDescription = document.getElementById("task-description").value;
+        const taskPriority = document.getElementById("task-priority").value;
+        const taskDueDate = document.getElementById("task-due-date").value;
+        const formatTaskDueDate = format(taskDueDate, 'MMMM dd, yyyy');
+
+        const myTodo = new Todo(taskTitle, taskDescription, formatTaskDueDate, taskPriority);
+        this.todoList.push(myTodo);
+    }
+
+    removeTodoList(e) {
+        const idToRemove = e.target.id;
+
+        if (e.target.checked) {
+            this.todoList = this.todoList.filter(todo => todo.id !== idToRemove);
+            const card  = e.target.closest('.todo-item');
+            if (card) {
+                card.classList.add('fade-out');
+                setTimeout(() => card.remove(), 300);
+            }
+        }
+    }
+}
