29e9b04b43c8f79d59e364eaa4ce928ca5efa6a8
Asshley <asshleydeanwaynegozar@gmail.com>
2025-06-16 18:29:22 +0800

    refactor: rename dom-generator into dom for folder name conciseness

---
+++ src/scripts/dom/project-generator.js
@@ -0,0 +1,18 @@
+
+export function newProject() {
+    // New feature: create new project directory | folder for organization
+    const newProject = document.createElement('div');
+
+    newProject.classList = 'project-folder';
+
+    return newProject;
+}
+
+export function newProjectItem() {
+    const newProjectItem = document.createElement('p');
+
+    newProjectItem.classList = 'project-item';
+
+    return newProjectItem;
+}
+
