23873e9ef1465996c10e4af9ffe7b96f1d19cf0b
Asshley <asshleydeanwaynegozar@gmail.com>
2025-06-19 00:09:30 +0800

    add new elements for folder container and items

--- src/scripts/dom/project-generator.js
+++ src/scripts/dom/project-generator.js
@@ -1,10 +1,17 @@
-export function newProject() {
+export function newProject(projectName) {
   // New feature: create new project directory | folder for organization
-  const project = document.createElement('div');
+  const projectFolder = document.createElement('div');
+  const projectFolderName = document.createElement('p');
+  const projectItemContainer = document.createElement('div');
+  
+  projectFolderName.textContent = projectName;
+  projectFolderName.classList = 'project-name';
+  projectItemContainer.classList = 'project-item-container';
+  projectFolder.classList = 'project-folder';
+  projectFolder.appendChild(projectFolderName);
+  projectFolder.appendChild(projectItemContainer);
 
-  project.classList = 'project-folder';
-
-  return project;
+  return projectFolder;
 }
 
 export function newProjectItem() {
