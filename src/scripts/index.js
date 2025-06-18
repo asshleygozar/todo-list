e492bb8535d86ae5d2b1e842ccc04c2b5ae4f633
Asshley <asshleydeanwaynegozar@gmail.com>
2025-06-19 00:07:27 +0800

    import todo project for rendering new projects upon dom loaded

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,18 @@
 import '../css/styles.css';
 import { addTodoModalCard } from './dom/todo-generator';
 import TodoModal from './ui/todo-modal';
+import TodoSideBar from './ui/todo-side-bar';
 
 document.addEventListener('DOMContentLoaded', () => {
   const bodyElement = document.querySelector('body');
   const modal = addTodoModalCard();
 
   const todoModal = new TodoModal(modal);
+  const sideBar = new TodoSideBar();
   const addNewTodoButton = document.getElementById('new-todo-button');
 
   bodyElement.appendChild(modal);
+  sideBar.renderNewProjects();
 
   addNewTodoButton.addEventListener('click', (e) => {
     e.stopPropagation();
