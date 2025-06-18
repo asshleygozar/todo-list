515ee2bcd1f3e2861aec52c9c05aab4a19955f77
Asshley <asshleydeanwaynegozar@gmail.com>
2025-06-18 13:34:56 +0800

    refactor: format code structure using prettier

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,63 +3,63 @@ import { addTodoModalCard, taskDetails } from './dom/todo-generator.js';
 import { TodoModal } from './ui/todo-modal.js';
 
 class TodoUpdater {
-	constructor(id) {
-		this.todoList = new TodoUIGenerator().todoList;
-		this.id = id;
-	}
+    constructor(id) {
+        this.todoList = new TodoUIGenerator().todoList;
+        this.id = id;
+    }
 
-	updatePriority(updatedPriority) {
-		for (let list of this.todoList) {
-			for (let todo in list) {
-				if (list.id == this.id) {
-					list.priority = updatedPriority;
-				}
-			}
-		}
-	}
+    updatePriority(updatedPriority) {
+        for (let list of this.todoList) {
+            for (let todo in list) {
+                if (list.id == this.id) {
+                    list.priority = updatedPriority;
+                }
+            }
+        }
+    }
 
-	updateTitle(updatedTitle) {
-		for (let list of this.todoList) {
-			for (let todo in list) {
-				if (list.id == this.id) {
-					list.title = updatedTitle;
-				}
-			}
-		}
-	}
+    updateTitle(updatedTitle) {
+        for (let list of this.todoList) {
+            for (let todo in list) {
+                if (list.id == this.id) {
+                    list.title = updatedTitle;
+                }
+            }
+        }
+    }
 
-	updateDescription(updatedDescription) {
-		for (let list of this.todoList) {
-			for (let todo in list) {
-				if (list.id == this.id) {
-					list.description = updatedDescription;
-				}
-			}
-		}
-	}
+    updateDescription(updatedDescription) {
+        for (let list of this.todoList) {
+            for (let todo in list) {
+                if (list.id == this.id) {
+                    list.description = updatedDescription;
+                }
+            }
+        }
+    }
 
-	updateDate(updatedDate) {
-		for (let list of this.todoList) {
-			for (let todo in list) {
-				if (list.id == this.id) {
-					list.dueDate = updatedDate;
-				}
-			}
-		}
-	}
+    updateDate(updatedDate) {
+        for (let list of this.todoList) {
+            for (let todo in list) {
+                if (list.id == this.id) {
+                    list.dueDate = updatedDate;
+                }
+            }
+        }
+    }
 }
 
 document.addEventListener('DOMContentLoaded', (e) => {
-	const bodyElement = document.querySelector('body');
-	const modal = addTodoModalCard();
+    const bodyElement = document.querySelector('body');
+    const modal = addTodoModalCard();
 
-	const todoModal = new TodoModal(modal);
-	const addNewTodoButton = document.getElementById('new-todo-button');
+    const todoModal = new TodoModal(modal);
+    const addNewTodoButton = document.getElementById('new-todo-button');
 
-	bodyElement.appendChild(modal);
+    bodyElement.appendChild(modal);
 
-	addNewTodoButton.addEventListener('click', (e) => {
-		e.stopPropagation();
-		todoModal.showModal();
-	});
+    addNewTodoButton.addEventListener('click', (e) => {
+        e.stopPropagation();
+        todoModal.showModal();
+    });
 });
