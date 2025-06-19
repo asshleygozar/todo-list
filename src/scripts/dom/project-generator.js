6158c683a243ebd466c4c598001d7bc2a57f6171
Asshley <asshleydeanwaynegozar@gmail.com>
2025-06-19 22:23:12 +0800

    add project item and tagged container dom elements function generator

--- src/scripts/dom/project-generator.js
+++ src/scripts/dom/project-generator.js
@@ -2,14 +2,11 @@ export function newProject(projectName) {
   // New feature: create new project directory | folder for organization
   const projectFolder = document.createElement('div');
   const projectFolderName = document.createElement('p');
-  const projectItemContainer = document.createElement('div');
-  
+
   projectFolderName.textContent = projectName;
   projectFolderName.classList = 'project-name';
-  projectItemContainer.classList = 'project-item-container';
   projectFolder.classList = 'project-folder';
   projectFolder.appendChild(projectFolderName);
-  projectFolder.appendChild(projectItemContainer);
 
   return projectFolder;
 }
@@ -20,4 +17,20 @@ export function newProjectItem() {
   projectItem.classList = 'project-item';
 
   return projectItem;
+}
+
+export function projectItemContent() {
+  const projectItemDiv = document.createElement('div');
+
+  projectItemDiv.classList.add('project-item-container');
+
+  return projectItemDiv;
+}
+
+export function projectTagItemsContainer() {
+  const projectTagContainer = document.createElement('div');
+
+  projectTagContainer.classList.add('task-tag-container');
+
+  return projectTagContainer;
 }
